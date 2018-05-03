9eeb07ff08ad590dc3df3bac48d965d4e68378ef
Caleb Fangmeier <caleb@fangmeier.tech>
2018-05-03 13:32:13 -0500

    Fixes distribution

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 
-with open('requirements.txt') as req:
-    install_requires = [l.strip() for l in req.readlines()]
-
 with open('README.md') as f:
     desc = f.read()
 
 setup(
+    author='Caleb Fangmeier',
+    author_email='caleb@fangmeier.tech',
+    url='https://github.com/cfangmeier/matplotboard',
     name='matplotboard',
-    version='0.5.0',
+    version='0.5.1',
     description='Generate simple HTML dashboards using matplotlib',
     long_description=desc,
     long_description_content_type='text/markdown',
-    install_requires=install_requires,
-    dependency_links=[
-        "git+ssh://git@github.com/cfangmeier/latexipy.git#egg=latexipy"
-    ],
+    install_requires=['matplotlib',
+                      'Jinja2',
+                      'Markdown',
+                      'python-markdown-math',
+                      'namedlist'],
     packages=['matplotboard'],
     package_data={'matplotboard': ['templates/*.j2',
                                    'static/css/*.css',
