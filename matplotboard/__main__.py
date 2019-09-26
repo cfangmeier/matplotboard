5cb8a4b0339b2f64d0a34240558bcad6ad703b17
Caleb Fangmeier <caleb@fangmeier.tech>
2019-09-26 15:37:16 -0500

    Improves dump navigation and adds output directory specification for command line tool.

--- matplotboard/__main__.py
+++ matplotboard/__main__.py
@@ -2,31 +2,35 @@ from .version import __version__
 
 
 def main():
+    from glob import glob
+    from os.path import join, split, splitext
     from argparse import ArgumentParser
-    from . import render, generate_report, loc_fig
+    from . import render, generate_report, loc_fig, configure
 
     parser = ArgumentParser("Matplotboard Command Line Utility")
+    parser.add_argument("dir", help="Directory containing png files to put in a dump")
     parser.add_argument(
         "--version",
         action="version",
         version="%(prog)s {version}".format(version=__version__),
     )
-    parser.add_argument("--dir", help="Directory containing png files to put in a dump")
+    parser.add_argument(
+        "--output",
+        help="Name of output directory.",
+        default="dashboard",
+    )
     args = parser.parse_args()
 
-    if args.dir:
-        from glob import glob
-        from os.path import join, split, splitext
+    configure(output_dir=args.output)
+    figures = {}
 
-        figures = {}
+    for f in glob(join(args.dir, "*.png")):
+        _, fname = split(f)
+        fig_name, ext = splitext(fname)
+        figures[fig_name] = loc_fig(f)
 
-        for f in glob(join(args.dir, "*.png")):
-            _, fname = split(f)
-            fig_name, ext = splitext(fname)
-            figures[fig_name] = loc_fig(f)
-
-        render(figures)
-        generate_report(figures, "Report")
+    render(figures)
+    generate_report(figures, "Report")
 
 
 if __name__ == "__main__":
